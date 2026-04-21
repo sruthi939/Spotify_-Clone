7e0a0ffaf29c2c6e6b5072ac3a37fe5e1b695057
sruthi939 <sathiyasuru@gmail.com>
2026-04-21 20:59:24 +0530

    Error fixing the player Context

---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,78 @@
+import { createContext, useEffect, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+const PlayerContext = createContext();
+
+const PlayerContextProvide = (props) => {
+
+    const audioRef = useRef();
+    const seekBg = useRef();
+    const seekBar = useRef();
+
+    const [track, setTrack] = useState(songsData[0]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time, setTime] = useState({
+        currentTime: {
+            second: 0,
+            mintue: 0
+        },
+        totalTime: {
+            second: 0,
+            minute: 0
+        }
+    })
+
+    const play = () => {
+        audioRef.current.play();
+        setPlayStatus(true)
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false)
+    }
+
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100)) + "%";
+                setTime({
+                    currentTime: {
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        mintue: Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime: {
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60)
+                    }
+                })
+            }
+        }, 1000);
+    }, [audioRef])
+
+const contextValue = {
+    audioRef,
+    seekBar,
+    seekBg,
+    track, setTrack,
+    playStatus, setPlayStatus,
+    time, setTime,
+    play,
+    pause,
+    playWithId
+}
+
+return (
+    <PlayerContext.Provide value={contextValue}>
+        {props.children}
+    </PlayerContext.Provide>
+)
+}
+
+export default PlayerContext
