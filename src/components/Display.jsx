4c6c4c2a96bfb054414a3b56ac081e1bcc81aadc
sruthi939 <sathiyasuru@gmail.com>
2026-04-21 19:18:14 +0530

    Fix the Display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useRef } from 'react'
-import { Routes, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import DisplayHome from './Home'
 import DisplayAlbum from './Album'
 import { albumsData } from '../assets/assets'
 
 const Display = () => {
+  const displayRef = useRef(null)
+  const location = useLocation()
 
-    const displayRef = useRef();
-    const location = useLocation();
-    const isAlbum = location.pathname.includes("album");
-    const albumId = isAlbum ? location.pathname.slice(-1) : "";
-    const bgColor = albumsData[Number(albumId)].bgColor;
+  const isAlbum = location.pathname.includes('/album/')
+  const albumId = isAlbum ? location.pathname.split('/').pop() : null
 
-    useEffect(()=>{
-        if (isAlbum) {
-            displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`
-        } else {
-            displayRef.current.style.background = `#121212`
-        }
-    })
+  const bgColor =
+    isAlbum && albumsData[Number(albumId)]
+      ? albumsData[Number(albumId)].bgColor
+      : '#121212'
+
+  useEffect(() => {
+    if (displayRef.current) {
+      displayRef.current.style.background = isAlbum
+        ? `linear-gradient(180deg, ${bgColor}, #121212 55%)`
+        : '#121212'
+    }
+  }, [isAlbum, bgColor])
 
   return (
-    <div className='w-full m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+    <div
+      ref={displayRef}
+      className='w-full m-2 px-6 pt-4 rounded-2xl text-white overflow-auto lg:w-[75%] lg:ml-0 transition-all duration-500'
+    >
       <Routes>
         <Route path='/' element={<DisplayHome />} />
         <Route path='/album/:id' element={<DisplayAlbum />} />
